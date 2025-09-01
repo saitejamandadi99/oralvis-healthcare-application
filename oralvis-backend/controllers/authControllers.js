1f39a3084465c44153a9fe1238cd74dac37c6843
saitejamandadi99 <saitejamandadi99>
2025-09-01 14:07:13 +0530

    created functions for user login and registration

---
+++ oralvis-backend/controllers/authControllers.js
@@ -0,0 +1,53 @@
+const dbPromise = require('../config/database');
+const {createUserTable} = require('../models/userModels');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.SECRET
+
+//Register User 
+const registerUser = async (req ,res ) =>{
+    try{
+        const {username, email, password} = req.body; 
+        const db = await dbPromise;
+        const ifuserExists = await db.get(`select * from users where email = ?`,[email]);
+        if(ifuserExists){
+            return res.status(400).json({message:"User already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUserInsert = `Insert into users (username, email, password) values(?,?,?)`;
+        const result = await db.run(newUserInsert,[username,email,hashedPassword]);
+        const UserId = result.lastID;
+        res.status(201).json({message:'User registered successfully'}); //user should login to get token after registration.
+
+
+    }catch(err){
+        console.error("Registration Error:", err);
+        res.status(500).json({message:"Server Error"});
+    }
+}
+
+//login user
+const loginUser = async (req,res) =>{ 
+    try{
+            const {email,password} = req.body;
+            const db = await dbPromise; 
+            const ifuserExists = await db.get(`select * from users where email = ?`, [email]);
+            if(!ifuserExists){
+                return res.status(400).json({message:"User with this email does not exist."});
+            }
+            const isPasswordValid = await bcrypt.compare(password,ifuserExists.password);
+            if(!isPasswordValid){
+                return res.status(400).json({message:"Invalid password"});
+            }
+            const token = jwt.sign({id:ifuserExists.id,email}, JWT_SECRET,{expiresIn:'2h'}) //token is used for authentication.
+            res.status(200).json({message:'Login successfully', token});
+    }
+    catch(err){
+        console.error("Login Error:", err);
+        res.status(500).json({message:"Server Error"});
+    }
+}
+
+module.exports = {
+    registerUser,loginUser
+};
---
+++ oralvis-backend/models/userModels.js
@@ -0,0 +1,21 @@
+const dbPromise = require('../config/database');
+
+const createUserTable = async () =>{
+    const db = await dbPromise;
+    const createTablequery = ` 
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP);`
+        await db.run(createTablequery);
+}
+
+    (createUserTable().catch((err)=>{
+    console.error("Error creating user table:", err);
+}));
+
+module.exports = {
+    createUserTable
+};
