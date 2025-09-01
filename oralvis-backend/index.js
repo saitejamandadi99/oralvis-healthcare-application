15840ac4951bbbf545f2248ed5cc73fe578d6ccd
saitejamandadi99 <saitejamandadi99>
2025-09-01 12:32:20 +0530

    updated the database into the server

--- oralvis-backend/index.js
+++ oralvis-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const dbPromise = require('./config/database');
 const app = express();
 app.use(express.json());
 app.use(cors());
